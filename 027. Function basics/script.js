f41bcefa6c415e3c0cf27bcf89da7ffa2d6d79f7
StefanW24 <90267126+StefanW24@users.noreply.github.com>
2021-12-02 19:43:51 +0100

    Add files via upload

---
+++ 027. Function basics/script.js
@@ -0,0 +1,21 @@
+const pizzaMaking = function () {
+    console.log("add sauce");
+    console.log("add salami");
+    console.log("add cheese");
+}
+
+// pizzaMaking();
+// pizzaMaking();
+// pizzaMaking();
+
+const sushiMaking = function () {
+    console.log("add fish");
+    console.log("add lobster");
+    console.log("add other weird stuff");
+}
+
+sushiMaking();
+sushiMaking();
+sushiMaking();
+pizzaMaking();
+pizzaMaking();
